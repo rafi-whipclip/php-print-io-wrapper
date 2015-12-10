f9a22b95bdc1ce0558795106d713eb5ca312de22
TalalM <talal@tvshowtime.com>
2015-12-10 18:46:10 +0100

    Added Guzzle Client and description of preconfiguredproducts endpoints

---
+++ src/resources/preconfiguredproducts.php
@@ -0,0 +1,122 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Operations
+    |--------------------------------------------------------------------------
+    |
+    | This array of operations is translated into methods that complete these
+    | requests based on their configuration.
+    |
+    */
+
+    "operations" => array(
+
+        /**
+         *    getPrintReadyProducts() method
+         *
+         *    reference: https://www.makeable.com/api-docs/curl/#print-ready-products
+         */
+        "getPrintReadyProducts" => array(
+            "httpMethod" => "GET",
+            "uri" => "preconfiguredproducts",
+            "summary" => "Retrieve a list of Print Ready Products",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "source" => array(
+                    "type" => "string",
+                    "location" => "query",
+                    "description" => "Description of the source platform",
+                    "default" => "api"
+                ),
+                "countryCode" => array(
+                    "type" => "string",
+                    "location" => "query",
+                    "description" => "The Ship To Country. Must be a 2 letter country short-name code (ISO 3166)",
+                    "default" => "FR"
+                ),
+            )
+        ),
+
+        /**
+         *    createPrintReadyProduct() method
+         *
+         *    reference: https://www.makeable.com/api-docs/curl/#print-ready-products
+         */
+
+        "createPrintReadyProduct" => array(
+            "httpMethod" => "POST",
+            "uri" => "preconfiguredproducts",
+            "summary" => "Insert a print ready (pre-configured) product into your recipe",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "SKU" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "Set a SKU that uniquely identifies the Print Ready product. This must be unique for each Print Ready Product",
+                    "required" => true,
+                ),
+                "Name" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "Set the name of the Print Ready Product",
+                    "required" => true,
+                ),
+                "Description" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "Set the description for the Print Ready Product",
+                    "required" => true,
+                ),
+                "Items" => array(
+                    "location" => "json",
+                    "parameters" => array(
+                        "ProductId" => array(
+                            "type" => "string",
+                            "location" => "json",
+                            "description" => "The ID of the product this item is a variant of",
+                            "required" => true,
+                        ),
+                        "ProductVariantSKU" => array(
+                            "type" => "string",
+                            "location" => "json",
+                            "description" => "The SKU of the product this item is a Print Ready version of",
+                            "required" => true,
+                        ),
+                        "Preconfigurations" => array(
+                            "location" => "json",
+                            "parameters" => array(
+                                "Url" => array(
+                                    "type" => "string",
+                                    "location" => "json",
+                                    "description" => "The url of the image in the referenced space/layer. This image should be formatted to the correct Print Ready Image size",
+                                    "required" => true,
+                                ),
+                            ),
+                            "required" => true,
+                        ),
+                    ),
+                    "required" => true,
+                )
+            ),
+        ),
+
+
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    |
+    | This array of models is specifications to returning the response
+    | from the operation methods.
+    |
+    */
+
+    "models" => array(
+
+    ),
+);
---
+++ src/resources/service-config.php
@@ -0,0 +1,56 @@
+<?php
+return array(
+
+	/*
+    |--------------------------------------------------------------------------
+    | Service Name
+    |--------------------------------------------------------------------------
+    |
+    | Name of the API service these description configs are for.
+    |
+    */
+
+    "name" => "Print.io",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Service Description
+    |--------------------------------------------------------------------------
+    |
+    | Description of the API service.
+    |
+    */
+
+    "description" => "A Print.io API Wrapper built using Guzzle",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Service Configurations
+    |--------------------------------------------------------------------------
+    |
+    | Configuration files of specfic service descriptions to load.
+    |
+    */
+
+    "services" => array(
+        "preconfiguredproducts",
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default models
+    |--------------------------------------------------------------------------
+    |
+    | Default response models for typical usage of responses
+    |
+    */
+
+    "models" => array(
+        "defaultJsonResponse" => array(
+            "type" => "object",
+            "additionalProperties" => array(
+                "location" => "json",
+            )
+        )
+    )
+);
